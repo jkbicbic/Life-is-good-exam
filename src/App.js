99becfcdc822ae62bb9ebdb3cc405fe39bd53b58
John Kennedy Bicbic <jkbicbic@gmail.com>
2020-04-08 14:51:28 +0800

    added components in app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import NavBar from './components/navbar/navbar';
+import Footer from './components/footer/footer';
+import Home from './components/home/home';
+import Other from './components/other-page/other-page';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="main">
+        <NavBar/>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/other' component={Other} />
+        </Switch>
+        <Footer/>
+      </div>
+    </Router>
   );
 }
 
